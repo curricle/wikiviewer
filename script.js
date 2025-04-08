04e5e1bbd7e8543f1a22b0afc2e93dc31eac7e6e
S. Jordan <curricle@gmail.com>
2025-04-08 14:38:32 -0400

    Update styling, add 'showing results' to page

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
         let url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&list=search&srsearch=" + term + "&format=json&callback=?";
 
         $.getJSON(url, function(data){
-            let html = ""; 
             let searchResults = data.query.search;
-            console.log(searchResults);
+            let html = "<div id='showing-results'>Showing " + searchResults.length + " results for <span id='search-term'>" + term + "</span>:</div>"; 
+            console.log(data.query);
             //create divs for each search result entry featuring article title and description
             $.each(searchResults, function(index, val){
                 //console.log(data);
-                html += "<a href=https://en.wikipedia.org/wiki/Special:Redirect/page/" + searchResults[index].pageid  + "><div class='pageDiv'>" + "<h2>" + searchResults[index].title + "</h2><p>" + searchResults[index].snippet + "...</p></div></a>";
+                html += "<a href=https://en.wikipedia.org/wiki/Special:Redirect/page/" + searchResults[index].pageid  + "><div class='search-item'>" + "<h2 class='search-item__title'>" + (index+1) + ". " + searchResults[index].title + "</h2><p class='search-item__description'>" + searchResults[index].snippet + "...</p></div></a>";
             });		
     
             //place search results into document html
